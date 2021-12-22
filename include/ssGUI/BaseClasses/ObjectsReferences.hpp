c35265d0744b47df7fcbf1ff2c909f40c30e5a58
Neko-Box-Coder <nekoboxcoder@gmail.com>
2021-12-22 17:35:23 +0000

    External ObjectReferences dependency management (bhX8X28u46)

--- include/ssGUI/BaseClasses/ObjectsReferences.hpp
+++ include/ssGUI/BaseClasses/ObjectsReferences.hpp
@@ -1,8 +1,7 @@
 #ifndef OBJECTS_REFERENCES
 #define OBJECTS_REFERENCES
 
-#include <vector>
-#include <queue>
+#include <unordered_map>
 
 namespace ssGUI
 {
@@ -10,24 +9,46 @@ namespace ssGUI
 
     class GUIObject;
     
+    // External Dependency
+    //     of B
+    //     ┌────────┐  References  ┌─────────┐
+    //     │        ├─────────────►│         │
+    //     │    A   │              │    B    │
+    //     │        │              │         │
+    //     │        ├─────────────►│         │
+    //     └────────┘Dependents On └─────────┘
+
     class ObjectsReferences
     {
         protected:
-            std::vector<ssGUI::GUIObject*> ObjectsReferencesTable;
-            std::queue<int> NextFreeObjectsReferenceIndices;
-            int ObjectsReferencesCount;
+            std::unordered_map<ssGUIObjectIndex, ssGUI::GUIObject*> ObjectsReferencesTable;
+            std::unordered_map<ssGUI::GUIObject*, ssGUIObjectIndex> ReverseObjectsReferencesTable;
+
+            ssGUIObjectIndex NextFreeIndex;
+            std::unordered_map<ObjectsReferences*, ssGUIObjectIndex> ExternalObjectsDependencies;
+            bool CleanedUp;
 
         public:
             ObjectsReferences();
-            ~ObjectsReferences() = default;
+            ObjectsReferences(ObjectsReferences const& other);
+            ~ObjectsReferences();
+
+            ObjectsReferences& operator=(ObjectsReferences const& other);
 
             virtual int GetObjectsReferencesCount() const;
+
             virtual ssGUIObjectIndex AddObjectReference(ssGUI::GUIObject* obj);
-            virtual ssGUI::GUIObject* GetObjectReference(ssGUIObjectIndex index) const;
+            virtual ssGUI::GUIObject* GetObjectReference(ssGUIObjectIndex index) const;          
             virtual void SetObjectReference(ssGUIObjectIndex index, ssGUI::GUIObject* obj);
+            virtual void RemoveObjectReference(ssGUIObjectIndex index);
+
+            virtual void AddExternalDependency(ObjectsReferences* dependency, ssGUIObjectIndex index);
+            virtual void RemoveExternalDependency(ObjectsReferences* dependency);
+
             virtual ssGUIObjectIndex GetObjectIndex(ssGUI::GUIObject* obj) const;
             virtual bool IsObjectReferenceExist(ssGUI::GUIObject* obj) const;
-            virtual void RemoveObjectReference(ssGUIObjectIndex index);
+
+            virtual void CleanUp();
     };
 }
 
