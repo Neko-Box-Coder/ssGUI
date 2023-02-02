06b7a7d9b79d2101b3e8646da604f20d0aa8051f
Neko Box Coder <nekoboxcoder@gmail.com>
2023-02-02 21:48:18 +0000

    Manual test for focus event

--- Src/Tests/ManualTests/EventCallbackTests/FocusedAndLostEventCallbackTest.cpp
+++ Src/Tests/ManualTests/EventCallbackTests/FocusedAndLostEventCallbackTest.cpp
@@ -13,9 +13,12 @@ void CleanUp()
 
 void Instructions()
 {
-    ssLOG_SIMPLE("Press 1 to change button interactable or not");
-    ssLOG_SIMPLE("Use mouse to interact with the button");
-    ssLOG_SIMPLE("You should be able to see button state change text in console when interacting with the button");
+    ssLOG_SIMPLE("Use mouse to interact with windows and widgets");
+    ssLOG_SIMPLE("You should be able to see focus change text in console");
+    ssLOG_SIMPLE("Press 1 to toggle window focus");
+    ssLOG_SIMPLE("Press 2 to toggle window2 focus");
+    ssLOG_SIMPLE("Press 3 to toggle widget focus");
+    ssLOG_SIMPLE("Press 4 to toggle widget2 focus");
 }
 
 int main()
@@ -30,19 +33,33 @@ int main()
     ssGUI::Widget widget;
     ssGUI::Widget widget2;
     
+    auto objToPtr = [&](void* ptr)
+    {
+        if(ptr == &window)
+            return "window";
+        else if(ptr == &window2)
+            return "window2";
+        else if(ptr == &widget)
+            return "widget";
+        else if(ptr == &widget2)
+            return "widget2";
+        else
+            return "unknown";
+    };
+    
     auto* ecb = ssGUI::Factory::Create<ssGUI::EventCallbacks::FocusedEventCallback>();
     auto* ecb2 = ssGUI::Factory::Create<ssGUI::EventCallbacks::FocusLostEventCallback>();
     ecb->AddEventListener(  "TestKey",    
-                            [](ssGUI::EventInfo info)
+                            [&](ssGUI::EventInfo info)
                             {
-                                ssLOG_SIMPLE("Focused: "<<info.EventSource);
+                                ssLOG_SIMPLE("Focused: "<<objToPtr(info.EventSource));
                                 assert(info.EventSource->IsFocused());
                             });
                             
     ecb2->AddEventListener(  "TestKey",    
-                            [](ssGUI::EventInfo info)
+                            [&](ssGUI::EventInfo info)
                             {
-                                ssLOG_SIMPLE("Focus lost: "<<info.EventSource);
+                                ssLOG_SIMPLE("Focus lost: "<<objToPtr(info.EventSource));
                                 assert(!info.EventSource->IsFocused());
                             });
     
@@ -65,10 +82,12 @@ int main()
     widget.SetParent(&mainWindow);
     widget2.SetParent(&window2);
 
-    ssLOG_SIMPLE("window (Red): "<<&window);
-    ssLOG_SIMPLE("window2 (Grey): "<<&window2);
-    ssLOG_SIMPLE("widget (Green): "<<&widget);
-    ssLOG_SIMPLE("widget2 (Blue): "<<&widget2);
+    ssLOG_SIMPLE("window: Red");
+    ssLOG_SIMPLE("window2: Grey");
+    ssLOG_SIMPLE("widget: Green");
+    ssLOG_SIMPLE("widget2: Blue");
+    ssLOG_SIMPLE("");
+    ssLOG_SIMPLE("");
 
     ssGUI::ssGUIManager manager;
     manager.AddGUIObject(&mainWindow);
@@ -78,7 +97,29 @@ int main()
         if( !inputInterface->IsButtonOrKeyPressExistLastFrame(ssGUI::Enums::NumberKey::ONE) &&
             inputInterface->IsButtonOrKeyPressExistCurrentFrame(ssGUI::Enums::NumberKey::ONE))
         {
-            //button.SetInteractable(!button.IsInteractable());
+            window.SetFocus(!window.IsFocused());
+            ssLOG_SIMPLE("window focus set to: "<<window.IsFocused());
+        }
+        
+        if( !inputInterface->IsButtonOrKeyPressExistLastFrame(ssGUI::Enums::NumberKey::TWO) &&
+            inputInterface->IsButtonOrKeyPressExistCurrentFrame(ssGUI::Enums::NumberKey::TWO))
+        {
+            window2.SetFocus(!window2.IsFocused());
+            ssLOG_SIMPLE("window2 focus set to: "<<window2.IsFocused());
+        }
+        
+        if( !inputInterface->IsButtonOrKeyPressExistLastFrame(ssGUI::Enums::NumberKey::THREE) &&
+            inputInterface->IsButtonOrKeyPressExistCurrentFrame(ssGUI::Enums::NumberKey::THREE))
+        {
+            widget.SetFocus(!widget.IsFocused());
+            ssLOG_SIMPLE("widget focus set to: "<<widget.IsFocused());
+        }
+        
+        if( !inputInterface->IsButtonOrKeyPressExistLastFrame(ssGUI::Enums::NumberKey::FOUR) &&
+            inputInterface->IsButtonOrKeyPressExistCurrentFrame(ssGUI::Enums::NumberKey::FOUR))
+        {
+            widget2.SetFocus(!widget2.IsFocused());
+            ssLOG_SIMPLE("widget2 focus set to: "<<widget2.IsFocused());
         }
     });
     
